af459e155415819eb5620e04c0cb0cc3a8cdc210
Jorge Rodriguez <jorge@lunarworkshop.com>
2015-02-19 16:10:10 -0800

    More getting rid of Hungarian.

--- common/math/maths.h
+++ common/math/maths.h
@@ -25,74 +25,74 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON A
 #include <math.h>
 #include <string.h>
 
-inline float Bias(float x, float flAmount)
+inline float Bias(float x, float amount)
 {
-	static float flLastAmount = -1;
-	static float flLastExp = -1;
+	static float last_amount = -1;
+	static float last_exp = -1;
 
-	if (flAmount == 0.5f)
+	if (amount == 0.5f)
 		return x;
 
-	if (flLastAmount != flAmount)
-		flLastExp = log(flAmount) * -1.4427f;	// 1/log(0.5f)
+	if (last_amount != amount)
+		last_exp = log(amount) * -1.4427f;	// 1/log(0.5f)
 
 	if (x < 0)
 		return 0;
 
-	return pow(x, flLastExp);
+	return pow(x, last_exp);
 }
 
-inline float Gain(float x, float flAmount)
+inline float Gain(float x, float amount)
 {
 	if(x < 0.5f)
-		return Bias(2*x, flAmount)/2;
+		return Bias(2 * x, amount) / 2;
 	else
-		return 1-Bias(2-2*x, flAmount)/2;
+		return 1 - Bias(2 - 2 * x, amount) / 2;
 }
 
 template <class T>
-inline const T LerpValue(const T& from, const T& to, float flLerp)
+inline const T LerpValue(const T& from, const T& to, float lerp)
 {
-	return to * flLerp + from * (1-flLerp);
+	return to * lerp + from * (1 - lerp);
 }
 
 template <class T>
-inline const T LerpValue(const T& from, const T& to, float flLerp, float flInLo, float flInHi)
+inline const T LerpValue(const T& from, const T& to, float lerp, float in_lo, float in_hi)
 {
-	return (((flLerp-flInLo) / (flInHi-flInLo)) * (to-from)) + from;
+	return (((lerp-in_lo) / (in_hi-in_lo)) * (to-from)) + from;
 }
 
 template <class T>
-inline const T LerpValueClamped(const T& from, const T& to, float flLerp, float flInLo, float flInHi)
+inline const T LerpValueClamped(const T& from, const T& to, float lerp, float in_lo, float in_hi)
 {
-	if (flLerp < flInLo)
+	if (lerp < in_lo)
 		return from;
 
-	if (flLerp > flInHi)
+	if (lerp > in_hi)
 		return to;
 
-	return (((flLerp-flInLo) / (flInHi-flInLo)) * (to-from)) + from;
+	return (((lerp-in_lo) / (in_hi-in_lo)) * (to-from)) + from;
 }
 
-inline float RemapVal(float flInput, float flInLo, float flInHi, float flOutLo, float flOutHi)
+inline float RemapVal(float input, float in_lo, float in_hi, float out_lo, float out_hi)
 {
-	return (((flInput-flInLo) / (flInHi-flInLo)) * (flOutHi-flOutLo)) + flOutLo;
+	return (((input-in_lo) / (in_hi-in_lo)) * (out_hi-out_lo)) + out_lo;
 }
 
-inline double RemapVal(double flInput, double flInLo, double flInHi, double flOutLo, double flOutHi)
+inline double RemapVal(double input, double in_lo, double in_hi, double out_lo, double out_hi)
 {
-	return (((flInput-flInLo) / (flInHi-flInLo)) * (flOutHi-flOutLo)) + flOutLo;
+	return (((input-in_lo) / (in_hi-in_lo)) * (out_hi-out_lo)) + out_lo;
 }
 
 template <class T>
-inline T RemapVal(T flInput, T flInLo, T flInHi, T flOutLo, T flOutHi)
+inline T RemapVal(T input, T in_lo, T in_hi, T out_lo, T out_hi)
 {
-	return (((flInput-flInLo) / (flInHi-flInLo)) * (flOutHi-flOutLo)) + flOutLo;
+	return (((input-in_lo) / (in_hi-in_lo)) * (out_hi-out_lo)) + out_lo;
 }
 
-inline double RemapBiased(double flInput, double flInLo, double flInHi, double flOutLo, double flOutHi, double flBias)
+inline double RemapBiased(double input, double in_lo, double in_hi, double out_lo, double out_hi, double flBias)
 {
-	return (Bias((float)((flInput - flInLo) / (flInHi - flInLo)), (float)flBias) * (flOutHi - flOutLo)) + flOutLo;
+	return (Bias((float)((input - in_lo) / (in_hi - in_lo)), (float)flBias) * (out_hi - out_lo)) + out_lo;
 }
 
 template <class T>
@@ -103,99 +103,99 @@ inline void TSwap(T& l, T& r)
 	r = temp;
 }
 
-inline float RemapValClamped(float flInput, float flInLo, float flInHi, float flOutLo, float flOutHi)
+inline float RemapValClamped(float input, float in_lo, float in_hi, float out_lo, float out_hi)
 {
-	float flReturn = RemapVal(flInput, flInLo, flInHi, flOutLo, flOutHi);
+	float flReturn = RemapVal(input, in_lo, in_hi, out_lo, out_hi);
 
-	if (flOutHi < flOutLo)
-		TSwap(flOutHi, flOutLo);
+	if (out_hi < out_lo)
+		TSwap(out_hi, out_lo);
 
-	if (flReturn < flOutLo)
-		return flOutLo;
+	if (flReturn < out_lo)
+		return out_lo;
 
-	if (flReturn > flOutHi)
-		return flOutHi;
+	if (flReturn > out_hi)
+		return out_hi;
 
 	return flReturn;
 }
 
-inline double RemapValClamped(double flInput, double flInLo, double flInHi, double flOutLo, double flOutHi)
+inline double RemapValClamped(double input, double in_lo, double in_hi, double out_lo, double out_hi)
 {
-	double flReturn = RemapVal(flInput, flInLo, flInHi, flOutLo, flOutHi);
+	double flReturn = RemapVal(input, in_lo, in_hi, out_lo, out_hi);
 
-	if (flOutHi < flOutLo)
-		TSwap(flOutHi, flOutLo);
+	if (out_hi < out_lo)
+		TSwap(out_hi, out_lo);
 
-	if (flReturn < flOutLo)
-		return flOutLo;
+	if (flReturn < out_lo)
+		return out_lo;
 
-	if (flReturn > flOutHi)
-		return flOutHi;
+	if (flReturn > out_hi)
+		return out_hi;
 
 	return flReturn;
 }
 
 template <class T>
-inline T RemapValClamped(float flInput, float flInLo, float flInHi, const T& flOutLo, const T& flOutHi)
+inline T RemapValClamped(float input, float in_lo, float in_hi, const T& out_lo, const T& out_hi)
 {
-	float flLower = std::min(flInLo, flInHi);
-	float flHigher = std::max(flInLo, flInHi);
+	float flLower = std::min(in_lo, in_hi);
+	float flHigher = std::max(in_lo, in_hi);
 
-	if (flInput < flLower)
-		return flOutLo;
+	if (input < flLower)
+		return out_lo;
 
-	if (flInput > flHigher)
-		return flOutHi;
+	if (input > flHigher)
+		return out_hi;
 
-	return ((flOutHi-flOutLo) * ((flInput-flInLo) / (flInHi-flInLo))) + flOutLo;
+	return ((out_hi-out_lo) * ((input-in_lo) / (in_hi-in_lo))) + out_lo;
 }
 
 template<typename T>
-T Clamp(T flInput, T flMin, T flMax)
+T Clamp(T input, T min, T max)
 {
-	return (flInput<flMin)?flMin:(flInput>flMax)?flMax:flInput;
+	return (input<min) ? min : (input>max) ? max : input;
 }
  
-inline float Blink(float flTime, float flLength)
+inline float Blink(float time, float length)
 {
-	if (fmod(flTime, flLength) > flLength/2)
+	if (fmod(time, length) > length / 2)
 		return 1.0f;
 	
 	return 0.0f;
 }
 
-inline float Oscillate(float flTime, float flLength)
+inline float Oscillate(float time, float length)
 {
-	return fabs(RemapVal((float)fmod(flTime, flLength), 0, flLength, -1, 1));
+	return fabs(RemapVal((float)fmod(time, length), 0, length, -1, 1));
 }
 
 // Strobe: Flicker("az", GetGameTime(), 0.1f)
 // Blink: Flicker("aaaaaaz", GetGameTime(), 1.0f)
 // Ramp: Flicker("abcdefghijklmnopqrstuvwxyzyxwvutsrqponmlkjihgfedcba", GetGameTime(), 1.0f)
-inline float Flicker(const char* pszValues, float flTime, float flLength)
+inline float Flicker(const char* letters, float time, float length)
 {
-	if (!pszValues)
+	if (!letters)
 		return 0;
 
-	size_t iValues = strlen(pszValues);
-	if (iValues == 0)
+	size_t num_values = strlen(letters);
+	if (num_values == 0)
 		return 0;
 
-	float flModTime = fmod(flTime, flLength);
-	int iValue = (int)RemapValClamped(flModTime, 0, flLength, 0, (float)iValues);
-	return RemapVal((float)pszValues[iValue], 'a', 'z', 0, 1);
+	float mod_time = fmod(time, length);
+	int value = (int)RemapValClamped(mod_time, 0, length, 0, (float)num_values);
+	return RemapVal((float)letters[value], 'a', 'z', 0, 1);
 }
 
-inline float Approach(float flGoal, float flInput, float flAmount)
+inline float Approach(float goal, float input, float amount)
 {
-	float flDifference = flGoal - flInput;
+	float flDifference = goal - input;
 
-	if (flDifference > flAmount)
-		return flInput + flAmount;
-	else if (flDifference < -flAmount)
-		return flInput -= flAmount;
+	if (flDifference > amount)
+		return input + amount;
+	else if (flDifference < -amount)
+		return input -= amount;
 	else
-		return flGoal;
+		return goal;
 }
 
 inline float AngleNormalize( float a )
@@ -226,16 +226,16 @@ inline float AngleDifference( float a, float b )
 	return d;
 }
 
-inline float AngleApproach(float flGoal, float flInput, float flAmount)
+inline float AngleApproach(float goal, float input, float amount)
 {
-	float flDifference = AngleDifference(flGoal, flInput);
+	float flDifference = AngleDifference(goal, input);
 
-	if (flDifference > flAmount)
-		return flInput + flAmount;
-	else if (flDifference < -flAmount)
-		return flInput -= flAmount;
+	if (flDifference > amount)
+		return input + amount;
+	else if (flDifference < -amount)
+		return input -= amount;
 	else
-		return flGoal;
+		return goal;
 }
 
 #endif
