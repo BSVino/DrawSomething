48c0af729851b4960d48f0ec7e408089972c45be
Jorge Rodriguez <bs.vino@gmail.com>
2015-04-26 20:00:57 -0700

    Use gl3.

--- common/tinker_gl.h
+++ common/tinker_gl.h
@@ -3,11 +3,14 @@
 #ifdef _WIN32
 #include <GL3/gl3w.h>
 #else
-#include <OpenGL/gl.h>
+#include <OpenGL/gl3.h>
 #endif
 
-#ifdef __APPLE__
-#define glBindVertexArray glBindVertexArrayAPPLE
-#define glGenVertexArrays glGenVertexArraysAPPLE
-
-#endif
+#define CheckGLReturn() \
+do { \
+	GLenum e = glGetError(); \
+	TAssert(e == GL_NO_ERROR); \
+	PRAGMA_WARNING_PUSH \
+	PRAGMA_WARNING_DISABLE(4127) /* conditional expression is constant */ \
+} while (0) \
+PRAGMA_WARNING_POP \
